b1df25cb8687b89a9913d54ef4db83066e9309b1
Sinawo Hlaleleni <47171705+SinawoHlaleleni@users.noreply.github.com>
2019-03-08 12:06:14 +0000

    Add files via upload

---
+++ assessmentProject3/src/main/java/assessmentProj3/Book.java
@@ -0,0 +1,63 @@
+package assessmentProj3;
+
+import javafx.util.Builder;
+
+import java.util.Objects;
+
+public class Book {
+
+    private int pageNum;
+    private String title;
+
+    private Book()
+    {
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static class Builder{
+        private int pageNum;
+        private String title;
+
+        public Builder pageNum(int k){
+            this.pageNum = k;
+            return this;
+        }
+
+        public Builder title(String k){
+            this.title=k;
+            return this;
+        }
+        public Book build()
+        {
+            return new Book(this);
+        }
+    }
+
+    public Book(Builder builder)
+    {
+        this.pageNum=builder.pageNum;
+        this.title=builder.title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(getPageNum(),book.getPageNum());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPageNum());
+    }
+}
+
+
